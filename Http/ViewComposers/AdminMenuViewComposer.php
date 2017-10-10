9554f99944930cc0718bbdc049b5e9a3ae8e246f
daniels.grietins <daniels.grietins@gmail.com>
2017-10-10 13:33:17 +0300

    Doc block

--- Http/ViewComposers/AdminMenuViewComposer.php
+++ Http/ViewComposers/AdminMenuViewComposer.php
@@ -9,7 +9,7 @@ class AdminMenuViewComposer{
     /**
      * Bind data to the view.
      *
-     * @param  View  $view
+     * @param  View  $view,
      * @return void
      */
     public function compose(View $view)
