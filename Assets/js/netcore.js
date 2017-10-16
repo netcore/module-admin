e3bcff5b5b9d7f31abe8802a663853a55c3b68dc
Kristers Zarins <kristers.zarins@netcore.lv>
2017-10-16 18:15:13 +0300

    Add .confirm-action to netcore.js

--- Assets/js/netcore.js
+++ Assets/js/netcore.js
@@ -40,7 +40,10 @@ $(document).ready(function () {
             }
         });
     });
-    
+
+    /**
+     * There also is .confirm-action down below, which is more sophisticated
+     */
     $('body').on('click', '.confirm-delete', function (e) {
         e.preventDefault();
         var btn = $(this);
@@ -75,6 +78,67 @@ $(document).ready(function () {
             }).fail(function () {
                 swal("Error", "Server error!", "error");
             });
+        }).catch(swal.noop);
+    });
+
+    /**
+     * This is improved version of .confirm-delete
+     * Probably .confirm-delete should be deprecated and deleted in favor of this
+     */
+    $('body').on('click', '.confirm-action', function (e) {
+        e.preventDefault();
+        var btn = $(this);
+
+        swal({
+            title: $(btn).data('title') || 'Confirmation',
+            text: $(btn).data('text') || 'Are you sure?',
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: $(btn).data('confirm-button-text') || 'Confirm',
+            cancelButtonText: $(btn).data('cancel-button-text') || 'Cancel'
+        }).then(function(){
+
+            $.ajax({
+                url: btn.data('href'),
+                type: btn.data('method'),
+                dataType: 'json',
+
+                success: function (response) {
+
+                    // Reload page on success
+                    var refreshPageOnSuccess = $(btn).data('refresh-page-on-success');
+                    if(refreshPageOnSuccess !== undefined && refreshPageOnSuccess !== false) {
+                        window.location.reload();
+                    }
+
+                    window.setTimeout(function () {
+                        swal({
+                            title: btn.data('success-title'),
+                            text: btn.data('success-text'),
+                            type: "success"
+                        });
+                    }, 100);
+
+                    var fadeOutSelector = btn.data('fade-out-selector');
+                    if ( fadeOutSelector && $(fadeOutSelector).length ) {
+                        $(fadeOutSelector).fadeOut();
+                    }
+
+                    var refreshDataTable = btn.data('refresh-datatable');
+                    if(refreshDataTable) {
+                        $(refreshDataTable).DataTable().ajax.reload();
+                    }
+                },
+                error: function (xhr) {
+                    console.log(xhr);
+                    swal("Error",
+                        "Sorry, there was an error. Please try again later. If the problem persists, please contact with technical support.",
+                        "error"
+                    );
+                }
+            });
+
         }).catch(swal.noop);
     });
 });
