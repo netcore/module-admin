95914edef65fce0909e2eca45d563b63c13f076b
speekadievs <arturs@54blue.com>
2017-11-17 11:49:35 +0200

    updated jquery url

--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -260,7 +260,7 @@
 </script>
 
 <!-- jQuery -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 <!-- Vue.Js -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.4.4/vue.js"></script>
