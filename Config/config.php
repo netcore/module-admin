33bd44c862d21e0aec0614cbb000090b6331027d
Dmitrijs Mihailovs <dimka62@inbox.lv>
2018-01-02 15:18:13 +0200

    Configurable date format

--- Config/config.php
+++ Config/config.php
@@ -2,25 +2,28 @@
 
 return [
     // Admin theme
-    'theme'     => env('ADMIN_THEME', 'candy-red'),
+    'theme'           => env('ADMIN_THEME', 'candy-red'),
 
     // Default login field to work with
-    'login'     => [
+    'login'           => [
         'username' => 'email'
     ],
 
     // User table and model
-    'user'      => [
+    'user'            => [
         'table' => 'users',
         'model' => \App\Models\User::class
     ],
 
     // IP whitelist
-    'whitelist' => [
+    'whitelist'       => [
         'enabled'     => false,
         'fallback_ip' => env('WHITELIST_FALLBACK_IP', '')
     ],
 
     // Show/hide copyrights text in footer
-    'show_copyrights' => true
+    'show_copyrights' => true,
+
+    // Global date format for data tables
+    'date_format'     => 'd.m.Y H:i'
 ];
