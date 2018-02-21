e8d4bc0802f5a5960885d26d8a6a06ab086ecade
Viktors Ņikiforovs <viktors.nikiforovs@netcore.lv>
2018-02-21 17:21:33 +0200

    seedItems() helper method update.

--- Repositories/MenuRepository.php
+++ Repositories/MenuRepository.php
@@ -2,7 +2,9 @@
 
 namespace Modules\Admin\Repositories;
 
+use Illuminate\Support\Collection;
 use Modules\Admin\Models\Menu;
+use Modules\Admin\Models\MenuItem;
 use Netcore\Translator\Helpers\TransHelper;
 
 /**
@@ -86,17 +88,16 @@ class MenuRepository
     {
         foreach ($menus as $menuData) {
             $menu = Menu::firstOrCreate(array_except($menuData, ['name']));
-
             $translations = [];
+
             foreach (TransHelper::getAllLanguages() as $language) {
                 $translations[$language->iso_code] = [
-                    'name' => $menuData['name']
+                    'name' => $menuData['name'],
                 ];
             }
+
             $menu->updateTranslations($translations);
         }
-
-
     }
 
     /**
@@ -107,22 +108,59 @@ class MenuRepository
     public function seedItems($menusWithItems)
     {
         foreach ($menusWithItems as $menuKey => $items) {
-            $menu = Menu::where('key', $menuKey)->first();
+            if ($menu = Menu::where('key', $menuKey)->first()) {
+                $this->seedItemsRecursively($menu, $items);
+            }
+        }
+    }
 
-            if ($menu) {
-                foreach ($items as $item) {
-                    $row = $menu->items()->create(array_except($item, ['name', 'value', 'parameters']));
+    /**
+     * Seed menu items with children recursively.
+     *
+     * @param \Modules\Admin\Models\Menu $menu
+     * @param array $items
+     * @param null|\Modules\Admin\Models\MenuItem $parent
+     * @return void
+     */
+    protected function seedItemsRecursively(Menu $menu, array $items, $parent = null)
+    {
+        foreach ($items as $item) {
+            $row = $menu->items()->create(
+                array_except($item, ['name', 'value', 'parameters', 'children'])
+            );
 
-                    $translations = [];
-                    foreach (TransHelper::getAllLanguages() as $language) {
-                        $translations[$language->iso_code] = [
-                            'name'       => $item['name'][$language->iso_code],
-                            'value'      => $item['value'][$language->iso_code],
-                            'parameters' => $item['parameters']
-                        ];
-                    }
-                    $row->updateTranslations($translations);
+            $translations = [];
+
+            foreach (TransHelper::getAllLanguages() as $language) {
+                $name = $item['name'];
+                $value = $item['value'];
+
+                // Check if name is passed as string or array with translations.
+                if (is_array($name) || $name instanceof Collection) {
+                    $name = $name[$language->iso_code];
                 }
+
+                // Check if value is passed as string or array with translations.
+                if (is_array($value) || $value instanceof Collection) {
+                    $value = $value[$language->iso_code];
+                }
+
+                $translations[$language->iso_code] = [
+                    'name'       => $name,
+                    'value'      => $value,
+                    'parameters' => $item['parameters'],
+                ];
+            }
+
+            $row->updateTranslations($translations);
+
+            // Child of parent.
+            if ($parent instanceof MenuItem) {
+                $parent->appendNode($row);
+            }
+
+            if (isset($item['children']) && is_array($item['children'])) {
+                $this->seedItemsRecursively($menu, $item['children'], $row);
             }
         }
     }
